520c457ca673b0da396a74d2cb04445a99f8c6cf
Michal Odnous <odiroot@users.noreply.github.com>
2023-02-27 13:00:02 +0000

    Add: Vector theme submodule.

--- LocalSettings.php
+++ LocalSettings.php
@@ -126,7 +126,8 @@ $wgGroupPermissions['*']['read'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "monobook";
+$wgDefaultSkin = "vector";
 
 # End of automatically generated settings.
 # Add more configuration options below.
+wfLoadSkin( 'Vector' );
