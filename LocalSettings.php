1d9a48c2ccf99491f21c638aeab2a5309ad615a8
Michal Odnous <odiroot@users.noreply.github.com>
2023-02-27 12:54:19 +0000

    Change: Fallback to default skin.

--- LocalSettings.php
+++ LocalSettings.php
@@ -126,7 +126,7 @@ $wgGroupPermissions['*']['read'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "monobook";
 
 # End of automatically generated settings.
 # Add more configuration options below.
